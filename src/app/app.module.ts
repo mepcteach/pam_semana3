bbf69e0ad05979de4b2149c5d1a0c22f9dfcf3d2
Miguel Puebla <mi.puebla@profesor.duoc.cl>
2024-10-27 19:42:52 -0300

    Sesión 3

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importa BrowserAnimationsModule
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,  BrowserAnimationsModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/registro/registro.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular'; 
+import { AlertController } from '@ionic/angular'; 
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage implements OnInit {
+
+  nombre: any='';
+  apellido: any='';
+  selectedOption: any=''; // nivel de estudios
+  selectedDate: any='';
+  usuario: any='';
+  password: any='';
+
+  constructor(private alertController: AlertController, private menu: MenuController) { }
+
+  ngOnInit() {
+    this.menu.close("mainMenu");
+  }
+
+  async presentAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Mensaje',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  guardar() {
+    if (this.nombre.trim() === '' || this.apellido.trim() === '') {
+      this.presentAlert('Error: nombre y apellido vacios');
+    } else {
+      this.presentAlert('Datos Correctos  usuario:  '+this.nombre+' fecha nacimiento: '+this.selectedDate); 
+    }
+  }
+
+}
